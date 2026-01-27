3db35fc1d01f1ea95781250823d8a9462187d86c
ratchanon <book.rcn24@gmail.com>
2026-01-27 16:23:56 +0700

    final push hp bar for 27/1/2569

--- lab/app/src/main/java/com/example/a526lablearnandroid/TamJai.kt
+++ lab/app/src/main/java/com/example/a526lablearnandroid/TamJai.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia.DefaultTab.PhotosTab.value
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.a526lablearnandroid.ui.theme._526LabLearnAndroidTheme
 
 class TamJai : ComponentActivity() {
@@ -32,10 +43,19 @@ class TamJai : ComponentActivity() {
 
 @Composable
 fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Huhuhehehaha $name!",
-        modifier = modifier
-    )
+    val inputtext = remember { mutableStateOf("") }
+    Column {
+        Text(
+            text = "Hello $name! say = " + inputtext.value,
+            modifier = modifier,
+        )
+        TextField(
+            value = inputtext.value,
+            onValueChange = {
+                inputtext.value = it
+            }
+        )
+    }
 }
 
 @Preview(showBackground = true)
